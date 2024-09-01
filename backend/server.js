0074773b7a834b59a1a4b74d82d7c4c842461aa4
2023-source <122154212+Priyansh218@users.noreply.github.com>
2024-09-01 17:17:35 +0530

    small change

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,9 @@ app.use(cors( { origin: 'http://localhost:3000',credentials: true,}))
 app.use(bodyParser.json());
 app.use('/uploads', express.static('uploads'));
 
+app.get("/",(req,res)=>{
+  res.json({message:"Hello World from backend"})
+})
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
