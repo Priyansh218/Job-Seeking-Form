3ff236b2c927a1f0c5f6e3c8020dc5617d299661
2023-source <122154212+Priyansh218@users.noreply.github.com>
2024-09-02 11:36:53 +0530

    small change

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors( { origin: 'http://localhost:3000',credentials: true,}))
+app.use(cors( { 
+  origin: ['http://localhost:3000','https://job-seeking-form-server1-pzlcnwn6x-priyansh-dahakes-projects.vercel.app'],
+  credentials: true,}))
 app.use(bodyParser.json());
 app.use('/uploads', express.static('uploads'));
 
